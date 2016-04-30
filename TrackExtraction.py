79f3f102a99218d366c92c2f0418f65629468399
iceboy95 <hqkhor95@gmail.com>
2016-05-01 01:45:22 +0800

    morphological transformation-dilation applied

--- TrackExtraction.py
+++ TrackExtraction.py
@@ -1,11 +1,33 @@
 import cv2
 import numpy
 import tkinter
+from matplotlib import pyplot as plt
 
 def TrackExtraction(filename,dataDir):
     video = dataDir + "\\" + filename
     videoReader = cv2.VideoCapture(video)
-    
+    #backgroundSubMOG = cv2.createBackgroundSubtractorMOG2()
+    backgroundSubKNN = cv2.createBackgroundSubtractorKNN()
+
+    while (videoReader.isOpened()):
+        ret, frame = videoReader.read()
+        #fgmask = backgroundSubMOG.apply(frame)
+        fgmask2 = backgroundSubKNN.apply(frame)
+        kernel = numpy.ones((5, 5))
+        # Dilation
+        dilation = cv2.dilate(fgmask2,kernel,iterations=1)
+        """
+        #opening
+        opening = cv2.morphologyEx(fgmask2,cv2.MORPH_OPEN,kernel)
+
+        # Erosion
+        erosion = cv2.erode(dilation,kernel,iterations=1)
+        """
+
+        cv2.imshow('backgroundSubKNN',dilation)
+        # waitKey is to control the speed of video, ord is to enable quit() using character
+        if cv2.waitKey(100) & 0xFF == ord('q'):
+            break
 
 
 
