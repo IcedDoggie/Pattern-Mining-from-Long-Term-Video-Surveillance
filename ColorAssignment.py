e3f98fbf707e6f0041cb2ad7760238cf6dacbeaa
iceboy95 <hqkhor95@gmail.com>
2017-03-22 01:02:57 +0800

    fixed color issues and removed unused codes

--- ColorAssignment.py
+++ ColorAssignment.py
@@ -17,21 +17,14 @@ def ColorAssignment(dataframe):
 #         dataframe = LinesConstruct(dataframe)        
         dataframe = dataframe.rename(columns={'TrackID': 'ClusterID'})
     
-
+#    print(dataframe['ClusterID'].as_matrix())
+    dataframe = dataframe['ClusterID'].as_matrix()
     ########################Assign Color##################
-    tempColor = np.where(dataframe['ClusterID'] == 0, 'blue', 
-                         np.where( dataframe['ClusterID'] == 1, 'yellow', 
-                                  np.where(dataframe['ClusterID'] == 2, 'cyan',
-                                           np.where(dataframe['ClusterID'] == 3, 'magenta','black'))))    
-    counter = 1
-#    helper_color = tempColor[0]
-    tempColor2 = []
-#    tempColor2 += [helper_color]
-#    while counter < len(tempColor):
-#        if helper_color != tempColor[counter]:
-#            tempColor2 += [tempColor[counter]]
-#            helper_color = tempColor[counter]
-#        counter += 1
-
+    tempColor = np.where(dataframe == 0, 'blue', 
+                         np.where( dataframe == 1, 'yellow', 
+                                  np.where(dataframe == 2, 'cyan',
+                                           np.where(dataframe == 3, 'magenta','black'))))    
+    tempColor = np.unique(tempColor)                       
+    print(tempColor)
     return tempColor
     ######################################################  
