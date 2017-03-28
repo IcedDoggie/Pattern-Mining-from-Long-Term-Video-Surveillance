b402d7d7cc99e4702fb0ef21d94b47c625ea0c41
iceboy95 <hqkhor95@gmail.com>
2017-03-29 00:40:00 +0800

    revised calculateSimilarity, note that this is to compute all traj with represent. traj., so the inner-outer loop sequence is exchanged

--- calculateSimilarity.py
+++ calculateSimilarity.py
@@ -17,21 +17,30 @@ def calculateSimilarity(representative_trajectory, new_trajectory, parameter_nj,
     new_trajectory, pivot_list = trajectory_pivoting_based_on_id(new_trajectory)
     num_new_trajectory = len(pivot_list) - 1
     new_trajectory = pd.DataFrame.as_matrix(new_trajectory)   
+    representative_trajectory, pivot_list2 = trajectory_pivoting_based_on_id(representative_trajectory) 
+    max_probability = np.empty([0])
+    representative_trajectory = pd.DataFrame.as_matrix(representative_trajectory)
+    representative_counter = 0
+
     
     probability_array = np.empty([0])
-    
+    threshold_to_compare = 0
     tracks_counter = 0
-    
-    while tracks_counter < num_new_trajectory: #T'
+    while tracks_counter < num_new_trajectory:
+        probability_array = np.empty([0])
+        representative_counter = 0
         one_track = new_trajectory[ pivot_list[tracks_counter]:pivot_list[tracks_counter+1] ]
-        
-        probability = [np.exp( (-parameter_nj) * chamfer_distance(one_track,representative_trajectory))]
-        if probability < threshold:
+        while representative_counter < len(pivot_list2) - 1: #T'
+            nj_param_current_cluster = parameter_nj[representative_counter]
+            
+            probability = [np.exp( (-nj_param_current_cluster) *
+                chamfer_distance(one_track, representative_trajectory[pivot_list2[representative_counter]:pivot_list2[representative_counter+1]]))]         
+            probability_array = np.append(probability_array, probability)
+            representative_counter += 1
+        max_probability = np.append(max_probability, max(probability_array)) #verify with dr.john    
+        threshold_to_compare = np.argmax(probability_array)        
+        if max_probability[tracks_counter] > threshold[threshold_to_compare]:
             num_anomaly += 1
-        
-        probability_array = np.append(probability_array, probability)
-        tracks_counter += 1
-#        tracks_counter += 1
-#     print(parameter_nj)
-    print(probability_array)
-    return probability, num_anomaly, probability_array, num_new_trajectory
+        tracks_counter += 1    
+    print(max_probability)
+    return probability, num_anomaly, max_probability, num_new_trajectory
